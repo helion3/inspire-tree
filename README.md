4df2374aa8b18fd32af50180235a8dd49284d09c
Mike Botsko <botsko@gmail.com>
2015-11-07 08:42:26 -0800

    Build via webpack

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 ## Inspire Tree
+
+
+
+### Development
+
+- `npm install` - Install all project dependencies.
+- `npm run setup` - Copy git hooks and run workspace setup.
+- `npm run watch` - Compile modules and run watcher for live updates.
+- `npm run lint` - Runs linters (should run on pre-commit as well).
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var path = require('path');
+
+var sassLoaders = [
+    'css-loader',
+    'autoprefixer-loader?browsers=last 5 versions',
+    'sass-loader?includePaths[]=' + path.resolve(__dirname, './src')
+];
+
+module.exports = {
+    entry: {
+        'inspire-tree': './src/tree.js'
+    },
+    output: {
+        filename: '[name].js',
+        path: './dist',
+        libraryTarget: 'umd'
+    },
+    module: {
+        loaders: [{
+            test: /\.scss$/,
+            loader: ExtractTextPlugin.extract('style-loader', sassLoaders.join('!'))
+        }, {
+            test: /\.css$/,
+            loader: 'style-loader!css-loader'
+        }, {
+            test: /\.woff/,
+            loader: 'file'
+        }, {
+            test: /\.woff2/,
+            loader: 'file'
+        }, {
+            test: /\.ttf/,
+            loader: 'file'
+        }, {
+            test: /\.eot/,
+            loader: 'file'
+        }, {
+            test: /\.svg/,
+            loader: 'file'
+        }, {
+            test: /\.png/,
+            loader: 'file'
+        }, {
+            test: /\.jpg/,
+            loader: 'file'
+        }]
+    },
+    plugins: [
+        new ExtractTextPlugin('[name].css')
+    ]
+};
