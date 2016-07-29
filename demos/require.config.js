5fbb88b86e64fe6056aacac1fe4e3c9dc9aafc1d
Mike Botsko <botsko@gmail.com>
2016-07-29 14:18:49 -0700

    Updates to latest lodash

--- demos/require.config.js
+++ demos/require.config.js
@@ -2,6 +2,6 @@ require.config({
     paths: {
         jquery: 'https://code.jquery.com/jquery-2.2.2.min',
         inspireTree: '../build/inspire-tree',
-        lodash: 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.6.1/lodash'
+        lodash: 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.14.1/lodash'
     }
 });
