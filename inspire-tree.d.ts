a351da1af361ce56d893dd246e53c21c5f614307
Mike Botsko <botsko@gmail.com>
2017-11-30 18:02:06 -0800

    Import EventEmitter2 type definition, d.ts cleanup. Fixes #174

--- inspire-tree.d.ts
+++ inspire-tree.d.ts
@@ -1,3 +1,5 @@
+import { EventEmitter2 } from 'eventemitter2';
+
 /**
  * Represents a generic callback which receives a single TreeNode argument.
  */
@@ -87,51 +89,90 @@ export interface Pagination {
     total: number;
 }
 
-export class InspireTree extends TreeNodes {
+export class InspireTree extends EventEmitter2 {
+    addNode(node: any): TreeNode;
     addNodes(node: Array<any>): TreeNodes;
+    available(): TreeNodes;
+    blur(): TreeNodes;
+    blurDeep(): TreeNodes;
     boundingNodes(): Array<TreeNode>;
     canAutoDeselect(): boolean;
+    checked(): TreeNodes;
+    clean(): TreeNodes;
     clearSearch(): InspireTree;
+    clone(): TreeNodes;
+    collapse(): TreeNodes;
+    collapsed(full?: boolean): TreeNodes;
+    collapseDeep(): TreeNodes;
     constructor(opts: Config);
+    constructor(tree: InspireTree, array: Array<any>|TreeNodes);
+    constructor(tree: InspireTree);
+    context(): TreeNode;
+    copy(hierarchy?: boolean): Copy;
     createNode(obj: any): TreeNode;
+    deepest(): TreeNodes;
+    deselect(): TreeNodes;
+    deselectDeep(): TreeNodes;
     disableDeselection(): InspireTree;
+    each(iteratee: NodeIteratee): TreeNodes;
+    editable(full?: boolean): TreeNodes;
+    editing(full?: boolean): TreeNodes;
     enableDeselection(): InspireTree;
+    expand(): TreeNodes;
+    expandDeep(): TreeNodes;
+    expanded(full?: boolean): TreeNodes;
+    expandParents(): TreeNodes;
+    extract(predicate: string|NodeIteratee): TreeNodes;
+    filterBy(predicate: string|NodeIteratee): TreeNodes;
+    flatten(predicate: string|NodeIteratee): TreeNodes;
+    focused(full?: boolean): TreeNodes;
+    get(index: number): TreeNode;
+    hidden(full?: boolean): TreeNodes;
+    hide(): TreeNodes;
+    hideDeep(): TreeNodes;
     id: string | number;
+    indeterminate(full?: boolean): TreeNodes;
+    insertAt(index: number, object: any): TreeNode;
+    invoke(methods: string|Array<string>): TreeNodes;
+    invokeDeep(methods: string|Array<string>): TreeNodes;
     isEventMuted(eventName: string): boolean;
     static isTreeNode(object: any): boolean;
     static isTreeNodes(object: any): boolean;
     lastSelectedNode(): TreeNode;
     load(loader: Promise<TreeNodes>): any;
+    loading(full?: boolean): TreeNodes;
+    matched(full?: boolean): TreeNodes;
+    move(index: number, newIndex: number, target: TreeNodes): TreeNode;
     mute(events: Array<string>): InspireTree;
     muted(): boolean;
+    node(id: string|number): TreeNode;
+    nodes(ids?: Array<string>|Array<number>): TreeNodes;
+    pagination(): Pagination;
+    recurseDown(iteratee: NodeIteratee): TreeNodes;
     reload(): Promise<TreeNodes>;
     removeAll(): InspireTree;
+    removed(full?: boolean): TreeNodes;
+    restore(): TreeNodes;
+    restoreDeep(): TreeNodes;
     search(query: string|RegExp|NodeIteratee): TreeNodes;
+    select(): TreeNodes;
+    selectable(full?: boolean): TreeNodes;
+    selectBetween(start: TreeNode, end: TreeNode): InspireTree;
+    selectDeep(): TreeNodes;
+    selected(full?: boolean): TreeNodes;
     selectFirstAvailableNode(): TreeNode;
+    show(): TreeNodes;
+    showDeep(): TreeNodes;
+    softRemove(): TreeNodes;
+    sortBy(sorter: string|NodeIteratee): TreeNodes;
+    state(key: string, val: boolean): TreeNodes;
+    stateDeep(key: string, val: boolean): TreeNodes;
+    swap(node1: TreeNode, node2: TreeNode): TreeNodes;
     toArray(): Array<any>;
+    toArray(): Array<any>;
+    tree(): InspireTree;
     unmute(events: Array<string>): InspireTree;
-
-    // Inherited from EventEmitter2.
-    // These are manually defined because otherwise we'd have to make
-    // the eventemitter2 module a dependency purely for the typings which
-    // would simply be spam for a majority of users
-    emit(event: string | string[], ...values: any[]): boolean;
-    emitAsync(event: string | string[], ...values: any[]): Promise<any[]>;
-    addListener(event: string, listener: Listener): this;
-    on(event: string | string[], listener: Listener): this;
-    prependListener(event: string | string[], listener: Listener): this;
-    once(event: string | string[], listener: Listener): this;
-    prependOnceListener(event: string | string[], listener: Listener): this;
-    many(event: string | string[], timesToListen: number, listener: Listener): this;
-    prependMany(event: string | string[], timesToListen: number, listener: Listener): this;
-    onAny(listener: EventAndListener): this;
-    prependAny(listener: EventAndListener): this;
-    offAny(listener: Listener): this;
-    removeListener(event: string | string[], listener: Listener): this;
-    off(event: string, listener: Listener): this;
-    removeAllListeners(event?: string | eventNS): this;
-    setMaxListeners(n: number): void;
-    eventNames(): string[];
+    visible(full?: boolean): TreeNodes;
 }
 
 export class TreeNodes extends Array<TreeNode> {
@@ -145,8 +186,8 @@ export class TreeNodes extends Array<TreeNode> {
     collapse(): TreeNodes;
     collapsed(full?: boolean): TreeNodes;
     collapseDeep(): TreeNodes;
-    constructor(tree: InspireTree);
     constructor(tree: InspireTree, array: Array<any>|TreeNodes);
+    constructor(tree: InspireTree);
     context(): TreeNode;
     copy(hierarchy?: boolean): Copy;
     deepest(): TreeNodes;
@@ -156,8 +197,8 @@ export class TreeNodes extends Array<TreeNode> {
     editable(full?: boolean): TreeNodes;
     editing(full?: boolean): TreeNodes;
     expand(): TreeNodes;
-    expanded(full?: boolean): TreeNodes;
     expandDeep(): TreeNodes;
+    expanded(full?: boolean): TreeNodes;
     expandParents(): TreeNodes;
     extract(predicate: string|NodeIteratee): TreeNodes;
     filterBy(predicate: string|NodeIteratee): TreeNodes;
@@ -193,8 +234,8 @@ export class TreeNodes extends Array<TreeNode> {
     state(key: string, val: boolean): TreeNodes;
     stateDeep(key: string, val: boolean): TreeNodes;
     swap(node1: TreeNode, node2: TreeNode): TreeNodes;
-    tree(): InspireTree;
     toArray(): Array<any>;
+    tree(): InspireTree;
     visible(full?: boolean): TreeNodes;
 }
 
@@ -209,9 +250,9 @@ export class TreeNode {
     clone(excludeKeys?: Array<string>): TreeNode;
     collapse(): TreeNode;
     collapsed(): boolean;
-    constructor(tree: InspireTree);
-    constructor(tree: InspireTree, source: any|TreeNode);
     constructor(tree: InspireTree, source: any|TreeNode, excludeKeys: Array<string>);
+    constructor(tree: InspireTree, source: any|TreeNode);
+    constructor(tree: InspireTree);
     context(): TreeNodes;
     copy(hierarchy?: boolean): Copy;
     copyHierarchy(excludeNode?: boolean): TreeNode;
@@ -232,15 +273,15 @@ export class TreeNode {
     hasOrWillHaveChildren(): boolean;
     hasParent(): boolean;
     hasVisibleChildren(): boolean;
-    hide(): TreeNode;
     hidden(): boolean;
-    indexPath(): string;
+    hide(): TreeNode;
     indeterminate(): boolean;
+    indexPath(): string;
     lastDeepestVisibleChild(): TreeNode;
     loadChildren(): Promise<TreeNodes>;
     loading(): boolean;
-    matched(): TreeNodes;
     markDirty(): TreeNode;
+    matched(): TreeNodes;
     nextVisibleAncestralSiblingNode(): TreeNode;
     nextVisibleChildNode(): TreeNode;
     nextVisibleNode(): TreeNode;
@@ -261,15 +302,15 @@ export class TreeNode {
     selected(): boolean;
     set(key: number|string, val: any): TreeNode;
     show(): TreeNode;
+    softRemove(): TreeNode;
     state(key: string, val: boolean): boolean;
     states(keys: Array<string>, val: boolean): boolean;
-    softRemove(): TreeNode;
     toggleCheck(): TreeNode;
     toggleCollapse(): TreeNode;
     toggleEditing(): TreeNode;
     toggleSelect(): TreeNode;
-    toString(): string;
     toObject(excludeChildren?: boolean, includeState?: boolean): any;
+    toString(): string;
     tree(): InspireTree;
     uncheck(shallow?: boolean): TreeNode;
     visible(): boolean;
