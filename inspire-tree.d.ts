0c4b03695e19214cbeb5e78d48673caabaeb7558
viveleroi <botsko@gmail.com>
2024-10-03 09:44:12 -0700

    Fixes type conflict between find and Array#find

--- inspire-tree.d.ts
+++ inspire-tree.d.ts
@@ -350,7 +350,7 @@ export class TreeNodes extends Array<TreeNode> {
     expandParents(): TreeNodes;
     extract(predicate: string|NodeIteratee): TreeNodes;
     filterBy(predicate: string|NodeIteratee): TreeNodes;
-    find(predicate: (node: TreeNode, index?: number, obj?: TreeNode[]) => boolean, thisArg?: any): TreeNode;
+    find(predicate: (node: TreeNode, index: number, obj: TreeNode[]) => boolean, thisArg?: any): TreeNode;
     flatten(predicate: string|NodeIteratee): TreeNodes;
     focused(full?: boolean): TreeNodes;
     get(index: number): TreeNode;
