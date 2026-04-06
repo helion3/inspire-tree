94c5c90858e2e51cef93be42fec6671f0d607af8
viveleroi <botsko@gmail.com>
2026-04-06 15:55:31 -0700

    Fixes rollup config for modern node

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,7 +4,7 @@ import commonjs from 'rollup-plugin-commonjs';
 import gzip from 'rollup-plugin-gzip';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import path from 'path';
-import pkgConfig from './package.json' assert { type: 'json' };
+import pkgConfig from './package.json' with { type: 'json' };
 import { uglify } from 'rollup-plugin-uglify';
 import { optimizeLodashImports } from '@optimize-lodash/rollup-plugin';
 
